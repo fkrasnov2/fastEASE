ceb75e1d643aa40e3521179cdd2851983ccd97c1
Fedor Krasnov <krasnov.fedor2@wb.ru>
2025-02-13 12:19:24 +0000

    Add example to README

--- Makefile
+++ Makefile
@@ -44,12 +44,12 @@ install: .venv .reports .base .main .extras
 	test -d dataset/ml-1m  || unzip dataset/ml-1m.zip -d dataset/
 
 .pytest:
-	pytest ${TESTS} --cov=${TESTS} --cov-report=xml
+	pytest ${TESTS}
 
 .lint: .isort .black .ruff
 lint: .venv .lint
 
-.test: .assets .pytest
+.test: .assets .extras .pytest
 test: .test
 
 build: 
--- tests/test_ml_1m.py
+++ tests/test_ml_1m.py
@@ -1,30 +1,52 @@
 import sys
+
 sys.path.append("src")
 
-from fastEASE import Dataset
-import  pytest
-from collections.abc import Iterable 
+from fastEASE import Dataset, PipelineEASE
+import pytest
+from collections.abc import Iterable
 
-class DatasetML1M (Dataset):
+
+class DatasetML1M(Dataset):
     def __init__(self, path_to_dataset: str):
-        super().__init__(self._load_interactions(path_to_dataset)) 
+        super().__init__(self.load_interactions(path_to_dataset))
 
-    def _load_interactions(self, path_to_dataset) -> Iterable[tuple[int,int]]:
+    @staticmethod
+    def load_interactions(path_to_dataset) -> Iterable[tuple[int, int]]:
         path_to_interactions = path_to_dataset + "/" + "ratings.dat"
         with open(path_to_interactions, "r") as file:
             for line in file:
                 yield tuple(map(int, line.strip("\n").split("::")[:2]))
-           
+
+
 @pytest.fixture
 def dataset_ml_1m():
     return DatasetML1M("dataset/ml-1m")
 
+
 def test_items_vocab(dataset_ml_1m):
     assert len(dataset_ml_1m.items_vocab) > 1000
+
 
 def test_users_vocab(dataset_ml_1m):
     assert len(dataset_ml_1m.users_vocab) > 500
 
-def test_interactions_matrix(dataset_ml_1m):
-    assert dataset_ml_1m.interactions_matrix.shape == (len(dataset_ml_1m.users_vocab), len(dataset_ml_1m.items_vocab))
 
+def test_interactions_matrix(dataset_ml_1m):
+    assert dataset_ml_1m.interactions_matrix.shape == (
+        len(dataset_ml_1m.users_vocab),
+        len(dataset_ml_1m.items_vocab),
+    )
+
+
+def test_ndcg():
+    pipeline = PipelineEASE(
+        user_item_it=DatasetML1M.load_interactions("dataset/ml-1m"),
+        min_item_freq=1,
+        min_user_interactions_len=5,
+        max_user_interactions_len=32,
+        calc_ndcg_at_k=True,
+        k=5,
+        predict_next_n=False,
+    )
+    assert pipeline.ndcg > 0.2
